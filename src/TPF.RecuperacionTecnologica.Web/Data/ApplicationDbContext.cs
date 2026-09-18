0bf1bb2231dd848271af428d6c6be90f73ab3d35
Kim <kimberly.blandong@gmail.com>
2026-09-18 18:55:54 -0300

    configuración: inicializar proyecto ASP.NET Core MVC

---
+++ src/TPF.RecuperacionTecnologica.Web/Data/ApplicationDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace TPF.RecuperacionTecnologica.Web.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
---
+++ src/TPF.RecuperacionTecnologica.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace TPF.RecuperacionTecnologica.Web.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
